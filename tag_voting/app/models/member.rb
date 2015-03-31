d81f3088b61146e0faf0905cf76a7b92b69ae1a2
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-31 14:00:52 -0500

    member.voted default to false

--- tag_voting/app/models/member.rb
+++ tag_voting/app/models/member.rb
@@ -2,6 +2,10 @@ class Member < ActiveRecord::Base
   has_many :ballots
   obfuscate_id
   attr_accessible :name, :email, :secret_voter_id, :voted
+  after_initialize :set_defaults
   
+  def set_defaults
+    self.voted ||= false
+  end
   
 end
