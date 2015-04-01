8657b9988541b985f002e7b3a699e19ae96cb97f
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-04-01 13:11:40 -0500

    update production config

--- tag_voting/config/environments/production.rb
+++ tag_voting/config/environments/production.rb
@@ -3,6 +3,18 @@ TagVoting::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  
+  Rails.application.routes.default_url_options[:host] = 'tag-voting-staging.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'tag.voting@gmail.com',
+    password:             'tagvoting9999',
+    authentication:       'plain',
+    :enable_starttls_auto => true
+  }
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
