04482cf675b97aaca5169f9e3ea78ff5bb8d7769
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-04-02 12:05:41 -0500

    cleaning precompile

--- tag_voting/config/application.rb
+++ tag_voting/config/application.rb
@@ -14,7 +14,7 @@ module TagVoting
   class Application < Rails::Application
     config.time_zone = 'Central Time (US & Canada)'
     config.active_record.default_timezone = 'Central Time (US & Canada)'
-    
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
