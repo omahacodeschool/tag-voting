6ffa05915b89a7c1d80bea51376d6099915a0582
Ruthie Peters <ruthie_peters@yahoo.com>
2015-04-03 10:55:13 -0500

    test_helper

--- tag_voting/test/functional/member_mailer_test.rb
+++ tag_voting/test/functional/member_mailer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MemberMailerTest < ActionMailer::TestCase
+  m = Member.create(:email => "anyemail@any.com")
+    
+    test "ballot_email" do
+      # Send the email, then test that it got queued
+      email = MemberMailer.ballot_email(m).deliver
+      assert !ActionMailer::Base.deliveries.empty?
+ 
+      # Test the body of the sent email contains what we expect it to
+      assert_equal ['tag.voting@gmail.com'], email.from
+      assert_equal ['anyemail@any.com'], email.to
+      assert_equal 'TAG Awards Nominations Now Open!', email.subject
+      # assert_equal read_fixture('invite').join, email.body.to_s
+    end
+  end
   # test "the truth" do
-  #   assert true
+  #   assert truth
   # end
-end
+
--- tag_voting/test/unit/helpers/members_helper_test.rb
+++ tag_voting/test/unit/helpers/members_helper_test.rb
@@ -2,3 +2,4 @@ require 'test_helper'
 
 class MembersHelperTest < ActionView::TestCase
 end
+
