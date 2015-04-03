558157c18f85f67c1e3d90743bb9be7012bb2a93
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-04-03 10:21:46 -0500

    written basic unit tests for ballots/members

--- tag_voting/test/unit/ballot_test.rb
+++ tag_voting/test/unit/ballot_test.rb
@@ -6,8 +6,30 @@ class BallotTest < ActiveSupport::TestCase
   # end
   
   test "closed?" do
-    vp = VotingPeriod.create(close_date: "2015-04-04 3:00pm")
-    Ballot.create(voting_period_id: vp.id)
+    vp = VotingPeriod.create(close_date: "2015-04-02 3:00pm")
+    ballot = Ballot.create(voting_period_id: vp.id)
+    assert( ballot.closed?) 
+    vp.destroy
+    ballot.destroy
+  end
+  
+  test "no_productions?" do
+    ballot = Ballot.create
+    assert( ballot.no_productions?)
+    ballot.productions.build
+    assert_equal(false, ballot.no_productions?)
+    ballot.productions.destroy
+    ballot.destroy
+  end
+  
+  test "has_seen?" do
+    production = Production.create
+    ballot = Ballot.create
+    assert_equal(false, ballot.has_seen?(production.id))
+    BallotsProduction.create(ballot_id: ballot.id, production_id: production.id)
+    assert_equal(true, ballot.has_seen?(production.id))
+    ballot.destroy
+    production.destroy
   end
   
 end
