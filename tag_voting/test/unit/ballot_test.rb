4aca5e8cfe3c95ec4ed9fb52c16ae1a02fffc9be
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-04-03 09:41:56 -0500

    beginning tests

--- tag_voting/test/unit/ballot_test.rb
+++ tag_voting/test/unit/ballot_test.rb
@@ -4,4 +4,10 @@ class BallotTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "closed?" do
+    vp = VotingPeriod.create(close_date: "2015-04-04 3:00pm")
+    Ballot.create(voting_period_id: vp.id)
+  end
+  
 end
