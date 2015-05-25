3ae35d9c1b3776bb67d95b8f5f52bc43c5321d08
Sumeet Jain <sumeet@sumeetjain.com>
2015-05-25 13:28:23 -0500

    MemberMailer fixes

--- tag_voting/app/admin/members.rb
+++ tag_voting/app/admin/members.rb
@@ -15,15 +15,16 @@ ActiveAdmin.register Member do
   filter :email
   filter :voted
   
-  members_array = []
   batch_action :email do |members|
-    members.each do |member|  
-      members_array << Member.find_by_id(member.to_i)
-        members_array.each do |member_object|
-          MemberMailer.ballot_email(member_object).deliver
-        end
-        redirect_to collection_path, alert: "The Email(s) were successfully sent"
+    members_array = Member.where(id: members)
+    
+    members_array.each do |member_object|
+      if !member_object.email.blank?
+        MemberMailer.ballot_email(member_object).deliver
+      end
     end
+    
+    redirect_to collection_path, alert: "The email(s) were successfully sent."
   end
   
   show do
